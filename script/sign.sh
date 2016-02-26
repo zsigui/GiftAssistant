d6a14effee1d089b68aabe593626bb919e6f9758
zhuangsigui <zhuangsigui@youmi.net>
2016-02-26 12:45:09 +0800

    修改apk签名脚本

--- script/sign.sh
+++ script/sign.sh
@@ -7,4 +7,4 @@ do
     fi
 done
 jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 keystore ../app/keystore/gift_cool.keystore -keypass ouwan_giftcool_key -storepass ouwan_giftcool_store -signedjar giftcool-unalign.apk app-release.encrypted.apk GiftCool
-zipalign -v 4 giftcool-unalign.apk giftcool-v1.0.3.apk
+zipalign -v 4 giftcool-unalign.apk giftcool-v1.0.4.apk
