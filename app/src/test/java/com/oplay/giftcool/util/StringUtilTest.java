4fe211c9d16eba3369a127b7e0429294d3dc2783
zhuangsigui <zhuangsigui@youmi.net>
2016-07-27 10:58:35 +0800

    fix

---
+++ app/src/test/java/com/oplay/giftcool/util/StringUtilTest.java
@@ -0,0 +1,31 @@
+package com.oplay.giftcool.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zsigui on 16-7-25.
+ */
+public class StringUtilTest {
+
+    @Test
+    public void testTransePhone() throws Exception {
+
+    }
+
+    @Test
+    public void testIsChinese() throws Exception {
+
+    }
+
+    @Test
+    public void testIsMessyCode() throws Exception {
+
+    }
+
+    @Test
+    public void testStringFilter() throws Exception {
+
+    }
+}
